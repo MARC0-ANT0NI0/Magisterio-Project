00a78cebfe99517118dfa816c017f6f5643694da
Docram <docram.desu@gmail.com>
2023-10-20 22:12:13 -0500

    Se creó un ".overlay" para oscurecer el body cuando aparezca el menu lateral

--- ScriptMG.js
+++ ScriptMG.js
@@ -1,5 +1,10 @@
 const b = document.getElementById("optionNav");
+const overlayConst = document.querySelector(".overlay");
 
 document.getElementById("menu-button").addEventListener("click", () =>  {
+    overlayConst.style.display = "block";
     b.classList.toggle("translate");
+    b.addEventListener("transitionend", e => {
+        if (!b.classList.contains("translate")) overlayConst.style.display = "none";
+    });
 });
